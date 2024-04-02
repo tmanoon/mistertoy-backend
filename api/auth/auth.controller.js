e20a6f054de011f11475df80da15acb90c487f71
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-02 17:01:17 +0300

    Edit functions in controller

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -19,13 +19,13 @@ export async function login(req, res) {
 
 export async function signup(req, res) {
     try {
-        const { username, password, fullname, isAdmin } = req.body
+        const { username, password, fullname, isAdmin, score } = req.body
         
         // IMPORTANT!!! 
         // Never write passwords to log file!!!
         // logger.debug(fullname + ', ' + username + ', ' + password)
         
-        const account = await authService.signup(username, password, fullname, isAdmin)
+        const account = await authService.signup(username, password, fullname, isAdmin, score)
         logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
         
         const user = await authService.login(username, password)
