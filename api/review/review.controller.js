90b3feaedb6306c83a2a52076be8628f588f25b9
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-02 17:01:36 +0300

    Edit functions in review controller

--- api/review/review.controller.js
+++ api/review/review.controller.js
@@ -35,27 +35,22 @@ export async function addReview(req, res) {
  
     try {
         let review = req.body
-        logger.info(review)
         review.userId = loggedinUser._id
         review = await reviewService.add(review)
         
         // prepare the updated review for sending out
-        review.aboutUser = await userService.getById(review.aboutUserId)
-        
         // Give the user credit for adding a review
         // var user = await userService.getById(review.byUserId)
         // user.score += 10
         loggedinUser.score += 10
-
         loggedinUser = await userService.update(loggedinUser)
-        review.byUser = loggedinUser
-
+        // review.byUser = loggedinUser
         // User info is saved also in the login-token, update it
         const loginToken = authService.getLoginToken(loggedinUser)
         res.cookie('loginToken', loginToken)
 
-        delete review.aboutUserId
-        delete review.byUserId
+        // delete review.aboutUserId
+        // delete review.byUserId
 
         // socketService.broadcast({type: 'review-added', data: review, userId: loggedinUser._id})
         // socketService.emitToUser({type: 'review-about-you', data: review, userId: review.aboutUser._id})
