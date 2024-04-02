09569677d1a6d299adcc3c5d44b3955cc7b78585
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-02 14:45:57 +0300

    Edit requiteAuth middleware to use als

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -1,29 +1,27 @@
-import { logger } from '../services/logger.service.js'
-import { authService } from '../api/auth/auth.service.js'
+import {config} from '../config/index.js'
+import {logger} from '../services/logger.service.js'
+import {asyncLocalStorage} from '../services/als.service.js'
 
-export async function requireAuth(req, res, next) {
-    if (!req?.cookies?.loginToken) {
-        return res.status(401).send('Not Authenticated')
-    }
-    
-    const loggedinUser = authService.validateToken(req.cookies.loginToken)
-    if (!loggedinUser) return res.status(401).send('Not Authenticated')
-
-    req.loggedinUser = loggedinUser
-    next()
+export function requireAuth(req, res, next) {
+  const { loggedinUser } = asyncLocalStorage.getStore()
+  req.loggedinUser = loggedinUser
+  
+  if (config.isGuestMode && !loggedinUser) {
+    req.loggedinUser = { _id: '', fullname: 'Guest' }
+    return next()
+  }
+  if (!loggedinUser) return res.status(401).send('Not Authenticated')
+  next()
 }
 
-export async function requireAdmin(req, res, next) {
-    if (!req?.cookies?.loginToken) {
-        return res.status(401).send('Not Authenticated')
-    }
-
-    const loggedinUser = authService.validateToken(req.cookies.loginToken)
-    if (!loggedinUser.isAdmin) {
-        logger.warn(loggedinUser.fullname + ' attempted to perform admin action')
-        res.status(403).end('Not Authorized')
-        return
-    }
-    req.loggedinUser = loggedinUser
-    next()
+export function requireAdmin(req, res, next) {
+  const { loggedinUser } = asyncLocalStorage.getStore()
+  if (!loggedinUser) return res.status(401).send('Not Authenticated')
+  if (!loggedinUser.isAdmin) {
+    logger.warn(loggedinUser.fullname + 'attempted to perform admin action')
+    res.status(403).end('Not Authorized')
+    return
+  }
+  next()
 }
+
